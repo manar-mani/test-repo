7704fa94c0e4664ed7b477b23b68747231194da6
[manar-mani] <[manarmani172@gmail.com]>
2023-01-27 19:43:05 +0100

    Add the needed codes for the redirection

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from user_agents import parse
 from werkzeug.user_agent import UserAgent
 import logging
+from os import path
 
 logging.basicConfig(filename='example.log', level=logging.DEBUG)
 
 app = Flask(__name__)
 
 # TODOs:
-# - Add requirements.txt file in order to install the required packages easily. 
+# - Add requirements.txt file in order to install the required packages easily. (Done)
 # - Check out the follwoing links to send data from Python to HTML and vice-versa:
-#   - https://pythonbasics.org/flask-template-data/
-#   - https://stackoverflow.com/questions/51669102/how-to-pass-data-to-html-page-using-flask
-#   - https://www.youtube.com/watch?v=aDOSQAq8cls&ab_channel=PythonProgramming
+#   - https://pythonbasics.org/flask-template-data/ (Done)
+#   - https://stackoverflow.com/questions/51669102/how-to-pass-data-to-html-page-using-flask (Done)
+#   - https://www.youtube.com/watch?v=aDOSQAq8cls&ab_channel=PythonProgramming (Done)
 
 # README
 # - In each file, you need to use sinigfic names
 # - Always add logger service !!!
 
 
-@app.route("/")
-def init():
+@app.route('/')
+def index():
     logging.info('***** Server running *****')
     ua = request.headers.get('User_Agent')
     logging.info(ua)
@@ -40,38 +41,47 @@ def init():
     logging.info('Device familly: '+str(user_agent.device.family))
     logging.info('Device brand: '+str(user_agent.device.brand))
     logging.info('Device model: '+str(user_agent.device.model))
+    
     return render_template('index.html')
+
+#def Install_Now():
+    #return render_template('Install_Now.html')
 
 
 
 # Why you do this? (what is the utility of button id?)
-def redirect(button_id='Install'):
-    ua = request.headers.get('User_Agent')
-    logging.info(ua)
-    ua_os = ua.os.family
-    if (ua.is_mobile == True):
-        match ua_os:
+@app.route('/install',methods = ['POST', 'GET'])
+def redirect():
+    user_agent = parse(request.headers.get('user_agent'))
+    logging.info(user_agent)
+    user_agent_os = user_agent.os.family
+    if (user_agent.is_mobile == True):
+        match user_agent_os :
             case "iOS":
-                button_id.href = "https://apps.apple.com/us/app/menutium/id1170811326"
+                return redirect(url_for('iOS'))
             case "Android":
-                button_id.href = "https://apps.apple.com/us/app/menutium/id1170811326"
+                 return redirect(url_for('Android'))
             case "HarmonyOS":
-                button_id.href = "https://apps.apple.com/us/app/menutium/id1170811326"
-    elif (ua.is_pc == True):
-        button_id.href = "{{ url_for('index') }}"
+                return redirect(url_for('HarmonyOS'))
+    elif (user_agent.is_pc == True):
+        return render_template('Install_Now.html')
     else:
-        button_id.href = "{{ url_for('index') }}"
+        return render_template('Install_Now.html')
 
 
 # Where is this file?
-# @app.route('/Install_Now')
-# def Install_Now():
-#    return render_template('Install_Now.html')
 
+def iOS():
+    return '<a>https://www.apple.com/app-store/</a>'
+def Android():
+    return '<a>https://www.apple.com/app-store/</a>'
+def HarmonyOS():
+    return '<a>https://www.apple.com/app-store/</a>'
 
-def redirect(button_id='button'):
-    return render_template("https://www.youtube.com/watch?v=ku5Zfa3KTWg")
-
+@app.route('/Install_Now')
+def Install_Now():
+    return render_template('Install_Now.html')
+    
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='192.168.31.218', port=8080, debug=True)
